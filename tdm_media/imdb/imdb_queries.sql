244c22a9ad6c5084c190830f815b56d12fdac733
Justin Thomas Mathew <mathew53@brown-a429.rcac.purdue.edu>
2021-10-14 13:06:10 -0400

    Justin Mathew Function get average rating for person_id

--- tdm_media/imdb/imdb_queries.sql
+++ tdm_media/imdb/imdb_queries.sql
@@ -14,3 +14,7 @@ WHERE
 			episode_title_id FROM episodes
 		WHERE
 			show_title_id = :title_id);
+            
+-- name: Justin_Mathew_01$
+-- Get the rating of the movie/tv episode/short with the given id
+SELECT AVG(ratings.rating) FROM ratings,crew,titles WHERE crew.title_id = titles.title_id and ratings.title_id = titles.title_id and crew.person_id = :person_id
